2458fc53e235d052f446681617c291ef4393edde
RKGoel <rajat.olf@gmail.com>
2023-12-10 01:36:39 -0800

    Test prompting with LangChains

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+from dotenv import load_dotenv, find_dotenv
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, SimpleSequentialChain, SequentialChain
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    load_dotenv(find_dotenv())
+    # print(os.getenv("OPENAI_API_KEY"))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # temperature param tunes creativity of model
+    llm = OpenAI(model_name="gpt-3.5-turbo-instruct", temperature=0.6)
+    # response = llm("explain large language model in one sentence")
+
+    prompt_template_name = PromptTemplate(
+        input_variables=['cuisine'],
+        template="I want to open a restaurant for {cuisine} food. Suggest a fancy name for it."
+    )
+
+    prompt_template_items = PromptTemplate(
+        input_variables=['restaurant_name'],
+        template="Suggest some food menu items for {restaurant_name}. Return it as a comma separated list."
+    )
+
+    ## Execute only one prompt with chain
+    restaurant_chain = LLMChain(llm=llm, prompt=prompt_template_name)
+    # restaurant_name = restaurant_chain.run("Indian")
+    # print(restaurant_name)
+
+    items_chain = LLMChain(llm=llm, prompt=prompt_template_items)
+    # items_name = items_chain.run(restaurant_name)
+    # print(items_name)
+
+    ## Execute prompts in simple sequential chain
+    # chain = SimpleSequentialChain(chains=[restaurant_chain, items_chain], verbose=True)
+    # items_from_simple_seq_chain = chain.run("Indian")
+    # print(items_from_simple_seq_chain)
+
+    # Execute prompts using sequential chain
+    restaurant_chain = LLMChain(llm=llm, prompt=prompt_template_name, output_key="restaurant_name")
+    items_chain = LLMChain(llm=llm, prompt=prompt_template_items, output_key="items_name")
+    chain = SequentialChain(
+        chains=[restaurant_chain, items_chain],
+        input_variables=['cuisine'],
+        output_variables=['restaurant_name', 'items_name']
+    )
+
+    items_from_seq_chain = chain({'cuisine':'Mexican'})
+    print(items_from_seq_chain)
